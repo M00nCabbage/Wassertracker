0690b5d8d8440d867152ca8547036249596d0f8c
Erik <reinscherik1@gmail.com>
2024-02-05 18:10:53 +0100

    erster Versuch einer API implementierung

--- app/src/main/java/com/example/sound1/MainActivity.java
+++ app/src/main/java/com/example/sound1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.sound1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -12,6 +13,14 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.Manifest;
 import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     private Button soundButton;
@@ -27,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
         textView=findViewById(R.id.text);
         soundButton=findViewById(R.id.SoundButton);
         shakeButton=findViewById(R.id.ShakeButton);
+
+        webrequest();
         //Sounderkennung soundDetectionService= new Sounderkennung();
 
 
@@ -50,6 +61,24 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent shakeIntent = new Intent(MainActivity.this, NewVibrationsSensor.class);
                 startActivity(shakeIntent);
+            }
+        });
+    }
+
+
+    private void webrequest(){
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request=new Request.Builder().url("http://132.180.217.157:5000").build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                textView.setText("nicht mit Server verbunden");
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                //textView.setText(response.body().string());
+                textView.setText("mit google verbunden");
             }
         });
     }
